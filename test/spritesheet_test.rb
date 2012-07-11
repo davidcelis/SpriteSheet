eb1e44b4bfc775224e4cabbb3cc813960cb54492
Nils Riedemann <nils.riedemann@gmail.com>
2012-07-11 17:44:06 +0200

    don't include the module

--- test/spritesheet_test.rb
+++ test/spritesheet_test.rb
@@ -3,7 +3,6 @@ require 'digest/md5'
 require File.expand_path('../../lib/spritesheet.rb', __FILE__)
 
 class SpriteSheetTest < Test::Unit::TestCase
-  include SpriteSheet
 
   def setup
     @sample_gif   = 'test/assets/example.gif'
@@ -12,43 +11,48 @@ class SpriteSheetTest < Test::Unit::TestCase
   end
 
   def test_takes_only_gif_as_argument
-    assert_raise ArgumentError do
-      sheet = Sheet.new 'donotcare.jpg'
+    assert_raise SpriteSheet::ArgumentError do
+      sheet = SpriteSheet::Sheet.new 'donotcare.jpg'
     end
   end
 
   def test_creates_imagelist
-    sheet = Sheet.new @sample_gif
+    sheet = SpriteSheet::Sheet.new @sample_gif
     assert_equal Magick::ImageList, sheet.frames.class
   end
 
   def test_sheet_width
-    sheet = Sheet.new @sample_gif
+    sheet = SpriteSheet::Sheet.new @sample_gif
     assert_equal ( 9*85 ), sheet.width
   end
 
   def test_height
-    sheet = Sheet.new @sample_gif
+    sheet = SpriteSheet::Sheet.new @sample_gif
     assert_equal 80, sheet.height
   end
 
   def test_sheet_frames
-    sheet = Sheet.new @sample_gif
+    sheet = SpriteSheet::Sheet.new @sample_gif
     assert_equal 9, sheet.frames.count
   end
 
   def test_creates_image
-    sheet = Sheet.new @sample_gif
+    sheet = SpriteSheet::Sheet.new @sample_gif
     assert_equal Magick::Image, sheet.sprite.class
     assert_equal sheet.width, sheet.sprite.columns
   end
 
   def test_write_image
-    sheet = Sheet.new @sample_gif
+    sheet = SpriteSheet::Sheet.new @sample_gif
     sheet.write_to @temp_sheet
     assert File.exists?(@temp_sheet), "There should have been a File generated"
     assert_equal Digest::MD5.hexdigest(File.read(@sample_sheet)),
       Digest::MD5.hexdigest(File.read(@temp_sheet))
+  end
+
+  def test_is_valid_file?
+    assert SpriteSheet::Sheet.is_valid?( @sample_gif ), "sample gif should be valid"
+    assert !SpriteSheet::Sheet.is_valid?( 'foobar.jpg' ), "jpg is not valid"
   end
 
   def teardown
